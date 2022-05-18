45b2a740465acab18a6c9f90ebc3dae3de4e0f77
Vardanyan <velison.vardanyan@teamsolutions.am>
2022-05-18 19:24:08 +0400

    initial

---
+++ src/test/java/com/awstest/awstest/AwstestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awstest.awstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwstestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
